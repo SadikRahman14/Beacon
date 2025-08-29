3b4a7d9f6f5abbb2285eec4fc57dc714e6eb0d5a
Sadik Rahman <sadik.nai.008@gmail.com>
2025-08-29 21:31:32 +0600

    Intial Commit

---
+++ Models/User.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Beacon.Models
+{
+    public class User : IdentityUser
+    {
+
+        [StringLength(100)]
+        public string? FirstName { get; set; }
+
+
+        [StringLength(100)]
+        public string? LastName { get; set; }
+
+        public string? ProfileImageUrl { get; set; }
+
+        // 'Role' can be a string or managed through IdentityRoles
+        [StringLength(50)]
+        public string Role { get; set; } = "citizen";
+
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [DataType(DataType.DateTime)]
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}

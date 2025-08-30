e7d5af21764625e354267e99e7433f80a8674a7c
Sumit Majumder <sumitmajumder270@gmail.com>
2025-08-31 01:34:05 +0600

    Stored and fetched profile and edit profile page information in database fixes #2

--- Models/User.cs
+++ Models/User.cs
@@ -6,10 +6,8 @@ namespace Beacon.Models
 {
     public class User : IdentityUser
     {
-
         [StringLength(100)]
         public string? FirstName { get; set; }
-
 
         [StringLength(100)]
         public string? LastName { get; set; }
@@ -18,6 +16,15 @@ namespace Beacon.Models
 
         [StringLength(50)]
         public string Role { get; set; } = "citizen";
+
+        [DataType(DataType.Date)]
+        public DateTime? DateOfBirth { get; set; }   
+
+        [StringLength(10)]
+        public string? Gender { get; set; }         
+
+        [StringLength(500)]
+        public string? Address { get; set; }         
 
         [DataType(DataType.DateTime)]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
